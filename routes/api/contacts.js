ec149511cb943ad42598a910329fab3ba30ceca6
Dmytro <109168390+TaraniukD@users.noreply.github.com>
2023-05-01 18:42:44 +0300

    Update contacts.js
    owner вирішено!

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -10,27 +10,31 @@ const router = express.Router();
 router.get('/', authenticate, async (req, res, next) => {
   try {
     const {_id: owner} = req.user;
-    const {page = 1, limit = 10} = req.query;
+    const {page = 1, limit = 10, favorite} = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "name email");
-  res.json(result)
+
+    const filterContact = favorite ? { owner, favorite } : { owner };
+    const result = await Contact.find(filterContact, "-createdAt -updatedAt", {skip, limit})
+    .populate("owner", "name email");
+  
+    res.json(result)
   } 
   catch (error) {
     next(error)
   }
 })
 
-router.get('/:contactId', isValidId, authenticate, async (req, res, next) => {
+router.get('/:contactId', authenticate, isValidId,  async (req, res, next) => {
  try {
-  const {contactId} = req.params;
-
-  const result = await Contact.findById(contactId);
-
-  if (!result) {
-   throw HttpError(404, "Not found")
+  const {_id: owner} = req.user;
+  const { contactId: _id } = req.params;
+  const response = await Contact.findOne({_id, owner});
+  if(response === null){
+    res.status(404).json({ message: 'Not found' })
   }
-
-  res.json(result)
+  else {
+  res.json(response);
+  };
 
  } 
  catch (error) {
@@ -64,8 +68,9 @@ router.put('/:contactId', authenticate, isValidId, async (req, res, next) => {
       throw HttpError(400, error.message)
     }
 
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+    const { contactId: _id } = req.params;
+    const {_id: owner} = req.user;
+    const result = await Contact.findOneAndUpdate({_id, owner}, req.body, {new: true});
     if(!result) {
       throw HttpError(404, "Not found");
     }
@@ -85,8 +90,9 @@ router.patch('/:contactId/favorite', authenticate, isValidId, async (req, res, n
       throw HttpError(400, error.message)
     }
 
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndUpdate(contactId, req.body, {new: true});
+    const { contactId: _id } = req.params;
+    const {_id: owner} = req.user;
+    const result = await Contact.findOneAndUpdate({_id, owner}, req.body, {new: true});
     if(!result) {
       throw HttpError(404, "Not found");
     }
@@ -100,8 +106,9 @@ router.patch('/:contactId/favorite', authenticate, isValidId, async (req, res, n
 
 router.delete('/:contactId', authenticate, isValidId, async (req, res, next) => {
   try {
-    const {contactId} = req.params;
-    const result = await Contact.findByIdAndDelete(contactId)
+    const {_id: owner} = req.user;
+    const { contactId: _id } = req.params;
+    const result = await Contact.findOneAndDelete({_id, owner})
     if(!result) {
       throw HttpError(404, "Not found");
     }
